fbf8a2150be2a8021efe89e645a52d3b54168767
Viktor Kjellberg <kjellberg.viktor@gmail.com>
2015-05-01 23:36:46 +0200

    9 java-packages: 5 done

--- incl/test/nine-gifts/index.php
+++ incl/test/nine-gifts/index.php
@@ -3,49 +3,120 @@
 <div id='flash'>
 
 <div id="box1" class="box">
+	<div class='minimize' title='Miminize this box'>x</div>
 	<img src="../../../img/giftbox.png" alt="" class="gift">
 	<h1 class="gift">1.</h1>
-	<p>Löptext</p>
+	<span class="unwrapped">
+		<h1>1. Jaha, här kan fixa en bakgrund dårå</h1>
+		<p>Ett gäng stycken med totalt meningslös text. Ja, alltså - textens innebörd är meningslös. Den får betydelse enbart genom att ta upp yta.</p>  
+		<p>Du kan nämligen klicka på ett stycke för att ändra dess bakgrund.</p>
+		<p>Coolt va?</p>
+		<p>Nja, kanske inte specielt. Men det ger det åtminstonde texten syfte. Inte fy skam för en text. </p>
+	</span>
 </div>
 <div id="box2" class="box">
+	<div class='minimize' title='Miminize this box'>x</div>
 	<img src="../../../img/giftbox.png" alt="" class="gift">
 	<h1 class="gift">2.</h1>
-	<p>Löptext</p>
+		<span class="unwrapped">
+		<img src='../../../img/me2.jpg?w=200' alt='' />
+		<h1>2. Bakgrund och bildstorlek</h1>
+		<p>Kolla vad skoj!. I det här exemplet kan vi både byta bakgrundsfärg OCH ändra storlek på bilden!!!</p>  
+	</span>
 </div>
 <div id="box3" class="box">
+	<div class='minimize' title='Miminize this box'>x</div>
 	<img src="../../../img/giftbox.png" alt="" class="gift">
 	<h1 class="gift">3.</h1>
-	<p>Löptext</p>
+	<span class="unwrapped">
+		<h1>3. Lägg till och ta bort element</h1>
+		<p>Ok, så här är det va. Skriv in nån skön färkod i hex i rutan och klicka på knappen för att lägga till den i paletten. Klicka på färgen och det tas bort. Magiskt!</p>
+		<label>Colorcode in hex (#eee or #5c5c5c): <input id='box3_color' type='text' name='color' /></label>
+		<input id='box3_add' type='button' value='Add color' /> 
+		</form></p>
+		<p id='palette'>Color Palette:</p>
+	</span>
 </div>
 <div id="box4" class="box">
+	<div class='minimize' title='Miminize this box'>x</div>
 	<img src="../../../img/giftbox.png" alt="" class="gift">
 	<h1 class="gift">4.</h1>
-	<p>Löptext</p>
+		<span class="unwrapped">
+		<img id='me-image-4' class='example' src='../../../img/me2.jpg?w=200' alt='' width='200px' height='254px' />
+		<h1>4. Selecting items, hide, show and change their look</h1>
+		<p>Aint it funny to be young. No worries here. I'm smiling :-D and I still have those shoes somewhere in the attic.</p>  
+		<p><form>
+		<input id='box4_dimensions' type='button' value='Get current dimensions wxh' /> <span id='box4_current'></span>
+		<br/>
+		<label>Width: </label>
+		<input id='box4_width_incr' type='button' value='+' />
+		<input id='box4_width_decr' type='button' value='-' />
+		<br/>
+		<label>Height: </label>
+		<input id='box4_height_incr' type='button' value='+' />
+		<input id='box4_height_decr' type='button' value='-' />
+		</form></p>
+	</span>
 </div>
 <div id="box5" class="box">
+	<div class='minimize' title='Miminize this box'>x</div>
 	<img src="../../../img/giftbox.png" alt="" class="gift">
 	<h1 class="gift">5.</h1>
-	<p>Löptext</p>
+		<span class="unwrapped">
+		<img id='me-image-5' class='example' src='../../../img/me2.jpg?w=200' alt='' width='200px' height='254px' />
+		<h1>5. Animera mera!</h1>
+		<p>Känn på dom här sköningarna. Web 2.0 - vår tids blinkande under construction ;-)</p>  
+		<p><a id='fadeToggle' href=#>Fade</a></p>
+		<p><a id='slideToggle' href=#>Slide</a></p>
+	</span></p>
 </div>
 <div id="box6" class="box">
+	<div class='minimize' title='Miminize this box'>x</div>
 	<img src="../../../img/giftbox.png" alt="" class="gift">
 	<h1 class="gift">6.</h1>
-	<p>Löptext</p>
+		<span class="unwrapped">
+		<h1>1. Selecting items, hide, show and change their look</h1>
+		<p>Aint it funny to be young. No worries here. I'm smiling :-D and I still have those shoes somewhere in the attic.</p>  
+		<p>This example is just to get going. You migh wonder how this box opens and closes? Its just a matter of hiding and showing the elements you want to display. The box minimizes itself when you click the x up in the right corner and you can open it up in full width by clicking the number or the wrapped giftbox.</p>
+		<p>When you click on the text or images in this example it will shift (toggle) between dark and light color.</p>
+		<p>Its all about using selectors and the eventhandlers.</p>
+	</span>
 </div>
 <div id="box7" class="box">
+	<div class='minimize' title='Miminize this box'>x</div>
 	<img src="../../../img/giftbox.png" alt="" class="gift">
 	<h1 class="gift">7.</h1>
-	<p>Löptext</p>
+		<span class="unwrapped">
+		<h1>1. Selecting items, hide, show and change their look</h1>
+		<p>Aint it funny to be young. No worries here. I'm smiling :-D and I still have those shoes somewhere in the attic.</p>  
+		<p>This example is just to get going. You migh wonder how this box opens and closes? Its just a matter of hiding and showing the elements you want to display. The box minimizes itself when you click the x up in the right corner and you can open it up in full width by clicking the number or the wrapped giftbox.</p>
+		<p>When you click on the text or images in this example it will shift (toggle) between dark and light color.</p>
+		<p>Its all about using selectors and the eventhandlers.</p>
+	</span>
 </div>
 <div id="box8" class="box">
+	<div class='minimize' title='Miminize this box'>x</div>
 	<img src="../../../img/giftbox.png" alt="" class="gift">
 	<h1 class="gift">8.</h1>
-	<p>Löptext</p>
+		<span class="unwrapped">
+		<h1>1. Selecting items, hide, show and change their look</h1>
+		<p>Aint it funny to be young. No worries here. I'm smiling :-D and I still have those shoes somewhere in the attic.</p>  
+		<p>This example is just to get going. You migh wonder how this box opens and closes? Its just a matter of hiding and showing the elements you want to display. The box minimizes itself when you click the x up in the right corner and you can open it up in full width by clicking the number or the wrapped giftbox.</p>
+		<p>When you click on the text or images in this example it will shift (toggle) between dark and light color.</p>
+		<p>Its all about using selectors and the eventhandlers.</p>
+	</span>
 </div>
 <div id="box9" class="box">
+	<div class='minimize' title='Miminize this box'>x</div>
 	<img src="../../../img/giftbox.png" alt="" class="gift">
 	<h1 class="gift">9.</h1>
-	<p>Löptext</p>
+		<span class="unwrapped">
+		<h1>1. Selecting items, hide, show and change their look</h1>
+		<p>Aint it funny to be young. No worries here. I'm smiling :-D and I still have those shoes somewhere in the attic.</p>  
+		<p>This example is just to get going. You migh wonder how this box opens and closes? Its just a matter of hiding and showing the elements you want to display. The box minimizes itself when you click the x up in the right corner and you can open it up in full width by clicking the number or the wrapped giftbox.</p>
+		<p>When you click on the text or images in this example it will shift (toggle) between dark and light color.</p>
+		<p>Its all about using selectors and the eventhandlers.</p>
+	</span>
 </div>
 <!--
 <input type="button" id="toggleButton" value="Hide" />
--- incl/test/nine-gifts/main.js
+++ incl/test/nine-gifts/main.js
@@ -1,8 +1,118 @@
 /**
  * Work with strings.
  */
+//$(document).ready(function(){
+// using short code instead...
 
 $(function(){
+	'use strict';
+  console.log('Ready to rumble!');
 
-  console.log('Ready');
+  $('.gift').click(function(){
+  	$(this).parent().addClass('fullWidth');
+  	$(this).parent().find('*').fadeIn('slow');
+  	$(this).parent().children('.gift').hide('slow');
+  	console.log("Opened a box.");
+  })
+
+  $('.minimize').click(function(){
+  	$(this).parent().find('*').hide();
+  	$(this).parent().removeClass().addClass('box').show();
+  	$(this).parent().children('.gift').fadeIn('slow');
+  	console.log("Minimezed a box.");
+  });
+
+
+  // Box 1
+  $('#box1 h1, #box1 p').click(function() {
+    $(this).toggleClass('dark');
+    console.log("toggle dark!");
+  });
+
+
+  // Box 2
+  $('#box2').click(function(){
+  	if($(this).hasClass('fullWidth')) {
+  		$(this).toggleClass('dark');
+  		console.log("toggle dark box!");
+  	}
+  });
+
+  $('#box2 .unwrapped img').click(function() {
+    $(this).toggleClass('thumbnail');
+    console.log("Resizing the image.");
+  });
+
+
+  // Box 3
+  var addColor = function() {
+  	$('<div></div>')
+	  	.addClass('palette')
+	  	.css('background-color', $('#box3_color').val())
+	  	.insertAfter('#palette')
+	  	.click(function(){
+	  		$(this).remove();
+	  		console.log("Removing a color.");
+	  	});
+  	console.log("Added a color.");
+  };
+
+  $('#box3 input[type=button]').click(addColor);
+  // Vad används stycket nedan till?
+	$('#box3 input[type=text]').keypress(function(event) {
+		if ( event.which == 13 ) {
+		  event.preventDefault();
+		  addColor();
+		}
+	});
+
+
+	// Box 4
+  var current_dimension = function() {
+    var height = $('#me-image-4').height(),
+      width = $('#me-image-4').width();
+    $('#box4_current').text(width + ' x ' + height);
+    console.log("Updated current dimensions on the image.");
+  };
+  
+  $('#box4_dimensions').click(current_dimension());
+  
+  $('#box4_height_incr').click(function() {
+    $('#me-image-4').css('height', '+=5px');
+    console.log("Increase the height of the image.");
+    current_dimension();
+  });
+  
+  $('#box4_height_decr').click(function() {
+    $('#me-image-4').css('height', '-=10px');
+    console.log("Decrease the height of the image.");
+    current_dimension();
+  });
+  
+  $('#box4_width_incr').click(function() {
+    $('#me-image-4').css('width', '+=4px');
+    console.log("Increase the width of the image.");
+    current_dimension();
+  });
+  
+  $('#box4_width_decr').click(function() {
+    $('#me-image-4').css('width', '-=8px');
+    console.log("Decrease the width of the image.");
+    current_dimension();
+  })
+
+
+  // Box 5
+  $('#fadeToggle').click(function(){
+  	$('#me-image-5').fadeToggle(2000);
+  });
+
+  $('#slideToggle').click(function(){
+  	$('#me-image-5').slideToggle(2000);
+  });
+
+
+  //
+ 
+  console.log('Main.js done!');
 });
